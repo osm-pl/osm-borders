7f3b6b781fa2e9dd708e71402bbde18aa30e2102
W <github@vink.pl>
2016-11-29 14:33:04 +0100

    Add commandline interface

---
+++ export_borders.py
@@ -0,0 +1,22 @@
+from borders.borders import get_borders
+import argparse
+import logging
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+            description="""Fetch administrative borders from EMUiA"""
+            )
+    parser.add_argument('--log-level', help='Set logging level (debug=10, info=20, warning=30, error=40, critical=50), default: 20', 
+                        dest='log_level', default=20, type=int)
+    parser.add_argument('--output', type=argparse.FileType('w+b'), default='result.osm',
+                            help='output file with merged data (default: result.osm)')
+
+    parser.add_argument('gmina', nargs=1, help='county terc code')
+    args = parser.parse_args()
+
+    logging.basicConfig(level=args.log_level)
+
+    args.output.write(get_borders(args.gmina[0]))
+
+if __name__ == '__main__':
+    main()
