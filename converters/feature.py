6a2f1e8ef85545ea385b9021f25c68b55633a32d
Wiktor Niesiobedzki <osm@vink.pl>
2016-12-13 00:48:00 +0100

    add immutable feature

--- converters/feature.py
+++ converters/feature.py
@@ -33,3 +33,18 @@ class Feature:
 
     def __str__(self):
         return str(self._tags)
+
+
+class ImmutableFeature:
+    def __init__(self, feature: Feature):
+        self.geometry = feature.geometry
+        self.tags = tuple(sorted(list(feature.tags.items())))
+
+    def __eq__(self, other):
+        return self.geometry == other.geometry and self.tags == other.tags
+
+    def __hash__(self):
+        return hash((self.geometry.wkt, self.tags))
+
+    def to_feature(self):
+        return Feature(self.geometry, dict(self.tags))
