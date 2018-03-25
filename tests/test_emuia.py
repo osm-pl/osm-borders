f5309deb1c93d189428bcb4f0ca9ec1f3d975b28
Wiktor Niesiobedzki <github@vink.pl>
2018-03-25 19:43:07 +0200

    Fix number of addresses.

--- tests/test_emuia.py
+++ tests/test_emuia.py
@@ -6,4 +6,4 @@ from converters import emuia
 class EmuiaTests(unittest.TestCase):
     def test_fetch(self):
         adresy = emuia.get_addresses("0601032")
-        self.assertEqual(3415, len(adresy))
+        self.assertEqual(4697, len(adresy))
